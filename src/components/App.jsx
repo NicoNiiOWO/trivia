b01de0fd169641484cf4bf1032e316811a1d9325
RoblesDaniel <danielhernandez@aclanyc.com>
2020-02-24 18:27:55 -0500

    trivia

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import '../css/App.css';
 import Question from './Question.jsx';
 import Answers from './Answers.jsx';
+import {buildFirebase} from '../clients/firebase.js';
 // import components
 
+function firebase() {
+var database = buildFirebase();
+var databaseRef = database.ref("/questions");
+databaseRef.once("value").then(function(data) {
+  const questions = data.val();
+  return questions;
+});
+}
+
+
 class App extends Component {
+  constructor (props){
+    super (props);
+    this.state = {questions:firebase()};
+  }
+
 
   render() {
     return (
       <div className="app">
         Trivia!
+        {this.state.questions}
         <Question question="1. Who is the realest JoJo?" answers={['Bartholomew Jojo Simpson', "Jojo Siwa", "Jojo Rabbit", "Jotaro Kujo"]}></Question>
+
         {/* // <Answers answer="Bartholomew Jojo Simpson"></Answers>
         // <Answers answer="Jojo Siwa"></Answers>
         // <Answers answer="Jojo Rabbit"></Answers>
